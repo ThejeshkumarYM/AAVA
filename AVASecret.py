022b9f2b166dec04cfbdfa3ec13371e0d08ce939
ThejeshkumarYM <thejesh.chinna@gmail.com>
2026-01-05 19:22:45 +0530

    Update AVASecret.py

--- AVASecret.py
+++ AVASecret.py
@@ -1,6 +1,1 @@
-_secrets = {
-    "GITHUB_TOKEN": "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"
-}
 
-def getValue(key: str):
-    return _secrets.get(key)
